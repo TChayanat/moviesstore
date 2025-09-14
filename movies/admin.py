69293b50ca69a88a2d3897df1fa9d53ef24c2740
TChayanat <tanja.chayanat@gmail.com>
2025-09-14 17:14:49 -0400

    Add likes

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Review
+from .models import Movie, Review, Like
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ class MovieAdmin(admin.ModelAdmin):
 	
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
+admin.site.register(Like)
